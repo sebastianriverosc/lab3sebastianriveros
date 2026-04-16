b744a4728ddf89ce14885d71b4b3e5eafc4a19d7
Your Name <you@example.com>
2026-04-16 20:10:29 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -59,7 +59,8 @@ HashMap * createMap(long capacity) {
 // No inserte claves repetidas. Recuerde que el arreglo es circular. Recuerde actualizar la variable size.
 
 void insertMap(HashMap * map, char * key, void * value) {
-
+    long pos = hash(key);
+    printf("%ld", pos);
 }
 
 // 3. Implemente la función Pair * searchMap(HashMap * map, char * key), la cual retorna el Pair asociado a la clave ingresada. 
