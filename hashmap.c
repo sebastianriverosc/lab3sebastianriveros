a71a2a5d710f28fa0d5ea126c99f0e75061cb11e
Your Name <you@example.com>
2026-04-18 20:32:24 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -101,11 +101,13 @@ Pair * searchMap(HashMap * map,  char * key) {
         while (map->buckets[pos] != NULL){
             pos++;
             if (strcmp(map->buckets[pos]->key, key) == 0){
+                map->current = pos;
                 return map->buckets[pos];
             }
         }
         return NULL;
     }
+    map->current = pos;
     return map->buckets[pos];
 }
 
